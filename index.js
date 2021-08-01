7e9ba383d26d63bcca4173ab9ea36e6c5a2724de
Shermam Miranda <shermam.miranda@goodeggs.com>
2021-08-01 17:04:04 -0300

    Handle bananaana

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = (word) => word.replace(/([AEIOUaeiou]\w+)\1$/, "$1");
+module.exports = (word) => word.replace(/(\w{2,})\1($|\W+)/, "$1$2");
--- index.test.js
+++ index.test.js
@@ -12,9 +12,12 @@ describe("removeEcho", () => {
     ${"astroastro"}    | ${"astro"}
     ${"a"}             | ${"a"}
     ${"bananaana"}     | ${"banana"}
+    ${"bananaanana"}     | ${"banana"}
     ${"banana"}        | ${"banana"}
     ${"BANANA"}        | ${"BANANA"}
     ${""}              | ${""}
+    ${"bananaana!"}    | ${"banana!"}
+    ${"bananaana!!"}   | ${"banana!!"}
   `("returns '$output' for input: '$input' ", ({ input, output }) => {
     expect(removeEcho(input)).toEqual(output);
   });
